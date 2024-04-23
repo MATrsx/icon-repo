45ec3dff52ced8e4c99c596e364f860f9a84bbe8
Milan <miliorti2017@gmail.com>
2024-04-23 11:51:16 +0200

    Adding fluent Emojis

--- Icons/script.py
+++ Icons/script.py
@@ -25,7 +25,36 @@ def create_json_from_svg_folder(folder_path, output_json_path):
 
     print("JSON-Datei wurde erfolgreich erstellt: output.json")
 
+def create_json_from_png_folder(base_path, output_json_path):
+    # Erstelle ein leeres Wörterbuch, um die Ergebnisse zu speichern
+    result_dict = {}
+
+    # Durchsuche die Ordnerstruktur nach PNG-Dateien
+    for root, dirs, files in os.walk(base_path):
+        for filename in files:
+            if filename.lower().endswith(".png"):
+                # Extrahiere den Icon-Namen aus dem Dateinamen und den Ordnername
+                icon_name = os.path.splitext(filename)[0]
+                icon_folder = os.path.relpath(root, base_path).replace("\\", "/")
+                parent_folder = os.path.basename(os.path.dirname(root))
+
+                # Erstelle den Schlüssel-Wert-Paar im gewünschten Format
+                key = f"fluentemoji-3d-{icon_name}"
+                value = f"fluentemoji/{os.path.join(icon_folder, filename)}".replace("\\", "/")
+                result_dict[key] = value
+
+    # Speichere das Wörterbuch als JSON-Datei
+    with open(output_json_path, "w") as json_file:
+        json.dump(result_dict, json_file, separators=(',', ':'))
+
+    print("JSON-Datei wurde erfolgreich erstellt:", output_json_path)
+
+# Beispielaufruf mit einem Basispfad
+base_path = "C:/Users/milan/Documents/Programming/PxC-Projects/icon-repo/Icons/fluentemoji"
+output_json_path = "C:/Users/milan/Documents/Programming/PxC-Projects/icon-repo/Icons/output.json"
+create_json_from_png_folder(base_path, output_json_path)
+
 # Beispielaufruf mit einem Ordnerpfad
-folder_path = "C:/Users/EMGZ2E/Git-Repositories/icon-repo/Icons/tabler/outline"
-output_json_path = "C:/Users/EMGZ2E/Git-Repositories/icon-repo/Icons/output.json"
-create_json_from_svg_folder(folder_path, output_json_path)
+# folder_path = "C:/Users/EMGZ2E/Git-Repositories/icon-repo/Icons/tabler/outline"
+# output_json_path = "C:/Users/EMGZ2E/Git-Repositories/icon-repo/Icons/output.json"
+# create_json_from_svg_folder(folder_path, output_json_path)
